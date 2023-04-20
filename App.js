7f279d887c43418e7560421ebd97250367ce3536
jai0323 <121802697+jai0323@users.noreply.github.com>
2023-04-20 18:54:41 +0530

    final components done

--- App.js
+++ App.js
@@ -1,12 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import FavoritesScreen from './screens/FavoritesScreen';
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import MealsDetailScreen from './screens/MealDetailScreen';
+import FavoritesContextProvider from './store/context/Favorites-context';
 
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+function DrawerNavigator(){
+  return <Drawer.Navigator 
+  screenOptions={
+    { headerStyle:{ backgroundColor:"#E55B3C"},
+      headerTintColor:'white',
+      sceneContainerStyle:{backgroundColor:'#fff'},
+      drawerContentStyle:{backgroundColor:"white"},
+      drawerActiveTintColor:"black",
+      drawerActiveBackgroundColor:"#FFCCCB",
+      drawerInactiveTintColor:"black"
+    }
+}
+  >
+  <Drawer.Screen
+   name="Categories"
+   component={CategoriesScreen}
+   options={{
+    title:"All Categories",
+    headerTitleAlign:"center",
+    drawerIcon:({color})=><Entypo name="list" size={24} color={color} />
+   }}
+   />
+   <Drawer.Screen
+   name="Favorites"
+   component={FavoritesScreen}
+   options={{
+    title:"Favorites",
+    headerTitleAlign:"center",
+    drawerIcon:({color})=><FontAwesome name="star" size={24} color={color} />
+   }}
+   />
+ </Drawer.Navigator>
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hi... i am merge conflict!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FavoritesContextProvider>
+   <NavigationContainer>
+    <Stack.Navigator screenOptions={
+    { headerStyle:{ backgroundColor:"#E55B3C"},
+      headerTintColor:'white',
+    }
+} >
+      <Stack.Screen name = "Drawer" component={DrawerNavigator} options={{headerShown:false}}/>
+      <Stack.Screen name = "MealsOverView" component={MealsOverviewScreen}/>
+      <Stack.Screen name = "MealDetailScreen" component={MealsDetailScreen}/>
+    </Stack.Navigator>
+   </NavigationContainer>
+   </FavoritesContextProvider>
   );
 }
 
--- data/dummy-data.js
+++ data/dummy-data.js
@@ -133,4 +133,205 @@ export const MEALS = [
     false,
     false
   ),
-]
+
+  new Meal(
+    'm5',
+    ['c2', 'c5', 'c10'],
+    'Salad with Smoked Salmon',
+    'luxurious',
+    'simple',
+    'https://cdn.pixabay.com/photo/2016/10/25/13/29/smoked-salmon-salad-1768890_1280.jpg',
+    15,
+    [
+      'Arugula',
+      "Lamb's Lettuce",
+      'Parsley',
+      'Fennel',
+      '200g Smoked Salmon',
+      'Mustard',
+      'Balsamic Vinegar',
+      'Olive Oil',
+      'Salt and Pepper'
+    ],
+    [
+      'Wash and cut salad and herbs',
+      'Dice the salmon',
+      'Process mustard, vinegar and olive oil into a dessing',
+      'Prepare the salad',
+      'Add salmon cubes and dressing'
+    ],
+    true,
+    false,
+    true,
+    true
+  ),
+
+  new Meal(
+    'm6',
+    ['c6', 'c10'],
+    'Delicious Orange Mousse',
+    'affordable',
+    'hard',
+    'https://cdn.pixabay.com/photo/2017/05/01/05/18/pastry-2274750_1280.jpg',
+    240,
+    [
+      '4 Sheets of Gelatine',
+      '150ml Orange Juice',
+      '80g Sugar',
+      '300g Yoghurt',
+      '200g Cream',
+      'Orange Peel'
+    ],
+    [
+      'Dissolve gelatine in pot',
+      'Add orange juice and sugar',
+      'Take pot off the stove',
+      'Add 2 tablespoons of yoghurt',
+      'Stir gelatin under remaining yoghurt',
+      'Cool everything down in the refrigerator',
+      'Whip the cream and lift it under die orange mass',
+      'Cool down again for at least 4 hours',
+      'Serve with orange peel'
+    ],
+    true,
+    false,
+    true,
+    false
+  ),
+
+  new Meal(
+    'm7',
+    ['c7'],
+    'Pancakes',
+    'affordable',
+    'simple',
+    'https://cdn.pixabay.com/photo/2018/07/10/21/23/pancake-3529653_1280.jpg',
+    20,
+    [
+      '1 1/2 Cups all-purpose Flour',
+      '3 1/2 Teaspoons Baking Powder',
+      '1 Teaspoon Salt',
+      '1 Tablespoon White Sugar',
+      '1 1/4 cups Milk',
+      '1 Egg',
+      '3 Tablespoons Butter, melted'
+    ],
+    [
+      'In a large bowl, sift together the flour, baking powder, salt and sugar.',
+      'Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.',
+      'Heat a lightly oiled griddle or frying pan over medium high heat.',
+      'Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.'
+    ],
+    true,
+    false,
+    true,
+    false
+  ),
+
+  new Meal(
+    'm8',
+    ['c8'],
+    'Creamy Indian Chicken Curry',
+    'pricey',
+    'challenging',
+    'https://cdn.pixabay.com/photo/2018/06/18/16/05/indian-food-3482749_1280.jpg',
+    35,
+    [
+      '4 Chicken Breasts',
+      '1 Onion',
+      '2 Cloves of Garlic',
+      '1 Piece of Ginger',
+      '4 Tablespoons Almonds',
+      '1 Teaspoon Cayenne Pepper',
+      '500ml Coconut Milk'
+    ],
+    [
+      'Slice and fry the chicken breast',
+      'Process onion, garlic and ginger into paste and sauté everything',
+      'Add spices and stir fry',
+      'Add chicken breast + 250ml of water and cook everything for 10 minutes',
+      'Add coconut milk',
+      'Serve with rice'
+    ],
+    true,
+    false,
+    false,
+    true
+  ),
+
+  new Meal(
+    'm9',
+    ['c9'],
+    'Chocolate Souffle',
+    'affordable',
+    'hard',
+    'https://cdn.pixabay.com/photo/2014/08/07/21/07/souffle-412785_1280.jpg',
+    45,
+    [
+      '1 Teaspoon melted Butter',
+      '2 Tablespoons white Sugar',
+      '2 Ounces 70% dark Chocolate, broken into pieces',
+      '1 Tablespoon Butter',
+      '1 Tablespoon all-purpose Flour',
+      '4 1/3 tablespoons cold Milk',
+      '1 Pinch Salt',
+      '1 Pinch Cayenne Pepper',
+      '1 Large Egg Yolk',
+      '2 Large Egg Whites',
+      '1 Pinch Cream of Tartar',
+      '1 Tablespoon white Sugar'
+    ],
+    [
+      'Preheat oven to 190°C. Line a rimmed baking sheet with parchment paper.',
+      'Brush bottom and sides of 2 ramekins lightly with 1 teaspoon melted butter; cover bottom and sides right up to the rim.',
+      'Add 1 tablespoon white sugar to ramekins. Rotate ramekins until sugar coats all surfaces.',
+      'Place chocolate pieces in a metal mixing bowl.',
+      'Place bowl over a pan of about 3 cups hot water over low heat.',
+      'Melt 1 tablespoon butter in a skillet over medium heat. Sprinkle in flour. Whisk until flour is incorporated into butter and mixture thickens.',
+      'Whisk in cold milk until mixture becomes smooth and thickens. Transfer mixture to bowl with melted chocolate.',
+      'Add salt and cayenne pepper. Mix together thoroughly. Add egg yolk and mix to combine.',
+      'Leave bowl above the hot (not simmering) water to keep chocolate warm while you whip the egg whites.',
+      'Place 2 egg whites in a mixing bowl; add cream of tartar. Whisk until mixture begins to thicken and a drizzle from the whisk stays on the surface about 1 second before disappearing into the mix.',
+      'Add 1/3 of sugar and whisk in. Whisk in a bit more sugar about 15 seconds.',
+      'whisk in the rest of the sugar. Continue whisking until mixture is about as thick as shaving cream and holds soft peaks, 3 to 5 minutes.',
+      'Transfer a little less than half of egg whites to chocolate.',
+      'Mix until egg whites are thoroughly incorporated into the chocolate.',
+      'Add the rest of the egg whites; gently fold into the chocolate with a spatula, lifting from the bottom and folding over.',
+      'Stop mixing after the egg white disappears. Divide mixture between 2 prepared ramekins. Place ramekins on prepared baking sheet.',
+      'Bake in preheated oven until scuffles are puffed and have risen above the top of the rims, 12 to 15 minutes.'
+    ],
+    true,
+    false,
+    true,
+    false
+  ),
+  new Meal(
+    'm10',
+    ['c2', 'c5', 'c10'],
+    'Asparagus Salad with Cherry Tomatoes',
+    'luxurious',
+    'simple',
+    'https://cdn.pixabay.com/photo/2018/04/09/18/26/asparagus-3304997_1280.jpg',
+    30,
+    [
+      'White and Green Asparagus',
+      '30g Pine Nuts',
+      '300g Cherry Tomatoes',
+      'Salad',
+      'Salt, Pepper and Olive Oil'
+    ],
+    [
+      'Wash, peel and cut the asparagus',
+      'Cook in salted water',
+      'Salt and pepper the asparagus',
+      'Roast the pine nuts',
+      'Halve the tomatoes',
+      'Mix with asparagus, salad and dressing',
+      'Serve with Baguette'
+    ],
+    true,
+    true,
+    true,
+    true
+  )
+];
